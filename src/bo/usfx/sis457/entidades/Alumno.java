97bdba3ccbde865b0c9a14db2615c339b07d0fbd
funny111tw <believefunny@gmail.com>
2018-05-11 00:35:00 -0400

    Clase modificada

--- src/bo/usfx/sis457/entidades/Alumno.java
+++ src/bo/usfx/sis457/entidades/Alumno.java
@@ -6,6 +6,7 @@
 package bo.usfx.sis457.entidades;
 
 import bo.usfx.sis457.Utilitarios;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -16,19 +17,26 @@ import java.util.GregorianCalendar;
 public class Alumno extends Persona {
     protected String CarnetUniversitario;
     protected int Semestre;
+    public String[] Asignaturas= new String[6];
     
-    public Alumno(String carnetUniversitario, int semestre, String carnetIdentidad, String nombre, Calendar fechaNacimiento) {
+    public Alumno(String []asignaturas, String carnetUniversitario, int semestre, String carnetIdentidad, String nombre, Calendar fechaNacimiento) {
         super(carnetIdentidad, nombre, fechaNacimiento);
         this.CarnetUniversitario = carnetUniversitario;
         this.Semestre = semestre;
+        this.Asignaturas = asignaturas;
         //System.out.println("Se crea un Alumno");
     }
     
-    public Alumno(String carnetUniversitario, int semestre) {
+    public Alumno(String []asignaturas, String carnetUniversitario, int semestre) {
         super();
         this.CarnetUniversitario = carnetUniversitario;
         this.Semestre = semestre;
+        this.Asignaturas = asignaturas;
         //System.out.println("Se crea un Alumno");
+    }
+
+    public Alumno(String asignaturas, String carnetUniversitario, int semestre, String carnetIdentidad, String nombre, Calendar fechaNacimientoCalendario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     public String getCarnetUniversitario() {
@@ -46,6 +54,20 @@ public class Alumno extends Persona {
     public void setSemestre(int semestre) {
         this.Semestre = semestre;
     }
+    public String getAsignaturas(String[] materias) {
+        StringBuffer Asignaturas=new StringBuffer();
+		for(int i=0; i<materias.length; i++){	
+			Asignaturas.append("  ");
+			Asignaturas.append(materias[i]);
+		}
+		return Asignaturas.toString();
+    }
+
+    public void setAsignaturas(String[] asignaturas) {
+        this.Asignaturas = asignaturas;
+    }
+
+   
 
     @Override
     public String toString() {
@@ -55,6 +77,7 @@ public class Alumno extends Persona {
                 + ", Nombre=" + this.Nombre 
                 + ", FechaNacimiento=" + Utilitarios.getFechaCalendario(this.FechaNacimiento)
                 + ", CarnetUniversitario=" + this.CarnetUniversitario 
-                + ", Semestre=" + this.Semestre + '}';
+                + ", Semestre=" + this.Semestre 
+                + ", Asignaturas=" + Arrays.toString(this.Asignaturas);
     }
 }
