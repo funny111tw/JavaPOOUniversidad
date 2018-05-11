94083555744edeffdb7f0f70612174a3e7f8b078
funny111tw <believefunny@gmail.com>
2018-05-11 00:31:07 -0400

    Clase administrativo creado

--- src/bo/usfx/sis457/entidades/Administrativo.java
+++ src/bo/usfx/sis457/entidades/Administrativo.java
@@ -5,10 +5,61 @@
  */
 package bo.usfx.sis457.entidades;
 
+import bo.usfx.sis457.Utilitarios;
+import java.util.Calendar;
+
 /**
  *
- * @author Marcelo
+ * @author Daniela Gonzales
  */
-public class Administrativo {
+public class Administrativo extends Persona{
+    protected String Cargo;
+    protected String LugarTrabajo;
+    protected Calendar FechaIngreso;
     
+   public Administrativo(String carnetIdentidad, String nombre, Calendar fechaNacimiento, String cargo, String lugartrabajo, Calendar fechaingreso){
+          super(carnetIdentidad, nombre, fechaNacimiento);
+          this.Cargo  = cargo;
+          this.LugarTrabajo = lugartrabajo;
+          this.FechaIngreso = fechaingreso;      
+   }
+   public Administrativo() {
+        
+    }
+    public String getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(String Cargo) {
+        this.Cargo = Cargo;
+    }
+
+    public Calendar getFechaIngreso() {
+        return FechaIngreso;
+    }
+
+    public void setFechaIngreso(Calendar FechaIngreso) {
+        this.FechaIngreso = FechaIngreso;
+    }
+
+    public String getLugarTrabajo() {
+        return LugarTrabajo;
+    }
+
+    public void setLugarTrabajo(String LugarTrabajo) {
+        this.LugarTrabajo = LugarTrabajo;
+    }
+   @Override
+    public String toString() {
+        return "Administrativo{" 
+                + ", Id=" + this.Id 
+                + ", CarnetIdentidad=" + this.CarnetIdentidad 
+                + ", Nombre=" + this.Nombre 
+                + ", FechaNacimiento=" + Utilitarios.getFechaCalendario(this.FechaNacimiento)
+                + ", Cargo=" + Cargo
+                + ", LugarTrabajo=" + LugarTrabajo
+                + ", FechaIngreso=" + Utilitarios.getFechaCalendario(this.FechaIngreso);      
+    }
+
 }
+
